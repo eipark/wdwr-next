98221c1e5703ca0ccab4bf874a81d4b4d7f0b899
Ernie Park <ernestipark@gmail.com>
2023-06-27 15:39:51 -0400

    styles

--- app/hero.tsx
+++ app/hero.tsx
@@ -1,10 +1,13 @@
 export default function Hero() {
   return (
-    <div className="full-width border-b-2 border-black">
+    <div className="full-width border-b-2 border-black mb-4">
       <div className="container items-center w-full mb-8 text-center">
         <h1>WE DONT WORK REMOTELY</h1>
-        <h3>Remote work is cool, but it's not for everyone.</h3>
-        <h3>The best <span className="underline decoration-sky-500">IN-PERSON</span> companies and jobs</h3>
+        <h3>The best 
+          <strong className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+            &nbsp;IN-PERSON&nbsp;
+          </strong>
+        companies and jobs</h3>
       </div>
     </div>
 
