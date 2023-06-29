46e7f931c26b983368051f834b61f00d3c7a04bd
Ernie Park <ernestipark@gmail.com>
2023-06-29 10:06:00 -0400

    add airtable

---
+++ app/employers/page.tsx
@@ -0,0 +1,32 @@
+export default function Employers() {
+  return (
+    <div className="container">
+      <h2 className="mb-4">Employers</h2>
+
+      <h3> List your company</h3>
+      <p>List your tech company on WDWR and get in front of great talent that right in your city. Fill out the form below. </p>
+
+      <script src="https://static.airtable.com/js/embed/embed_snippet_v1.js"></script>
+      <iframe
+        className="airtable-embed airtable-dynamic-height" 
+        src="https://airtable.com/embed/shrYmUdmr51DEgZCo?backgroundColor=orange"
+        frameBorder={"0"}
+        onmousewheel=""
+        height="1350"
+        width="100%"
+        style={{background: "transparent",  border: "1px solid #ccc"}}
+      />
+
+
+      <h3>Post a job</h3>
+      <p>Coming soon! If you're interested, drop a line to wedontworkremotely@gmail.com</p>
+
+
+
+
+
+
+
+    </div>
+  );
+}
