89c09488526f2a5fa35a677309f8679dc22d151c
Ernie Park <ernestipark@gmail.com>
2023-06-29 09:33:26 -0400

    nav

--- app/nav.tsx
+++ app/nav.tsx
@@ -6,7 +6,8 @@ import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
 
 const navigation = [
-  { name: 'Employers', href: '#', current: true },
+  { name: 'Employers', href: '/employers', current: true },
+  { name: 'About', href: '/About', current: false },
 ]
 
 function classNames(...classes) {
@@ -64,9 +65,13 @@ export default function Example({ user }) {
                         {item.name}
                       </Link>
                     ))}
+                    <Link href="/employers" className="rounded-md px-3 py-2 text-sm font-medium bg-white">
+                      + List your company
+                    </Link>
                   </div>
                 </div>
               </div>
+
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
                 {user ? (
