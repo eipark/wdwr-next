d39c2f0db1f40a8aad8db60ac9227789b43cf847
Ernie Park <ernestipark@gmail.com>
2023-06-29 11:09:32 -0400

    citypicker oclumns

--- app/cityPicker.tsx
+++ app/cityPicker.tsx
@@ -3,12 +3,16 @@ import Link from 'next/link'
 
 export default function CityPicker({ cities }) {
   return (
-    <div className="container">
+    <div className="container sm:columns-1 md:columns-3 text-white">
       <ul>
       {cities?.map((city) => (
-        <li key={city.id}><Link href={`/cities/${city.abbreviation.toLowerCase()}`}>{city.name}</Link></li>
+        <div key={city.id} className="w-full gap-8 mb-5 p-3 bg-black border-solid border-zinc-200 border-2">
+          <Link href={`/cities/${city.abbreviation.toLowerCase()}`}>{city.name}
+          </Link>
+        </div>
       ))}
       </ul>
     </div>
   );
-}
+};
+
